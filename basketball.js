1fe0a55a10908fd60a3be16acc81fe8f0c6a17b5
André Mateus <95716954+Andre-Mateus-Naveca@users.noreply.github.com>
2022-10-18 18:04:34 -0400

    Add files via upload

---
+++ basketball.js
@@ -0,0 +1,51 @@
+status1 = false;
+function preload()
+{
+    img = loadImage("BolaDeBasquete.jpg");
+}
+function setup()
+{
+    canvas = createCanvas(640, 420)
+    canvas.center()
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detectando Objetos";
+}
+function modelLoaded() {
+    console.log("Modelo Carregado!")
+    status1 = true;
+    objectDetector.detect(img, gotResults)
+}
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
+function voltar()
+{
+    window.location = "index.html"
+}
+function draw()
+{
+    image(img, 0, 0, 640, 420)
+    
+        if(status1 != ""){
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status: Objetos Detectados";
+
+            fill("red")
+
+            percent = floor(objects[i].confidence * 100)
+
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+
+            noFill()
+
+            stroke("red")
+
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+    }
+}
+}
---
+++ main.js
@@ -0,0 +1,20 @@
+function anchor1()
+{
+    window.location = "armario.html"
+}
+function anchor2()
+{
+    window.location = "basketball.html"
+}
+function anchor3()
+{
+    window.location = "bottle.html"
+}
+function anchor4()
+{
+    window.location = "clock.html"
+}
+function anchor5()
+{
+    window.location = "fan.html"
+}
